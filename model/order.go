095f289d005754bc07638090b7f3d831d1f54047
Zamy03 <ukasyzzam@gmail.com>
2024-11-06 11:06:37 +0700

    modified order to transaksi

--- model/order.go
+++ model/order.go
@@ -6,22 +6,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// Order struct
-type Order struct {
-	ID       primitive.ObjectID `json:"order_id,omitempty" bson:"_id,omitempty"`
-	Quantity int                `json:"quantity,omitempty" bson:"quantity,omitempty"`
-	Payments []Payment          `json:"payments,omitempty" bson:"payments,omitempty"`
-}
-
-// Payment struct untuk data pembayaran
-type Payment struct {
-	ID             primitive.ObjectID `json:"payment_id,omitempty" bson:"_id,omitempty"`
-	OrderID        primitive.ObjectID `json:"order_id,omitempty" bson:"order_id,omitempty"`
-	TotalPrice     float32            `json:"total_price,omitempty" bson:"total_price,omitempty"`
-	PaymentDate    time.Time          `json:"payment_date,omitempty" bson:"payment_date,omitempty"`
-	PaymentProof   string             `json:"payment_proof,omitempty" bson:"payment_proof,omitempty"`
-	Status         string             `json:"status,omitempty" bson:"status,omitempty"`
-	PaymentMethodID primitive.ObjectID `json:"payment_method_id,omitempty" bson:"payment_method_id,omitempty"` // Menambahkan ID metode pembayaran
-	CreatedAt      time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt      time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+// transaction struct
+type Transaction struct {
+	ID           primitive.ObjectID `json:"transaction_id,omitempty" bson:"_id,omitempty"`
+	UserID       primitive.ObjectID `json:"user_id,omitempty" bson:"user_id,omitempty"`
+	MerchantID   primitive.ObjectID `json:"merchant_id,omitempty" bson:"merchant_id,omitempty"`
+	PaymentMethodID primitive.ObjectID `json:"payment_method_id,omitempty" bson:"payment_method_id,omitempty"`
+	Amount       float32            `json:"amount,omitempty" bson:"amount,omitempty"`
+	Currency     string             `json:"currency,omitempty" bson:"currency,omitempty"`
+	Status       string             `json:"status,omitempty" bson:"status,omitempty"`
+	Description  string             `json:"description,omitempty" bson:"description,omitempty"`
+	CreatedAt	time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt	time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
