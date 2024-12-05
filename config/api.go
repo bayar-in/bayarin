d3af306f608955f7341a30cf5b492a3dfade9082
MUHAMMAD RIFKY <muhammad26rifky06@gmail.com>
2024-12-05 13:20:46 +0700

    + db

--- config/api.go
+++ config/api.go
@@ -27,3 +27,5 @@ var PRIVATEKEY string = "e4cb06d20bcce42bf4ac16c9b056bfaf1c6a5168c24692b38eb46d5
 var APIGETPDLMS string = "https://pamongdesa.kemendagri.go.id/webservice/public/user/get-by-phone?number="
 
 var APITOKENPD string = os.Getenv("PDTOKEN")
+
+var PUBLICKEY string = "47db091df55d64499fdf2ca85504ac4d320c4c645c9bef75efac0494314cae94"
--- config/db.go
+++ config/db.go
@@ -8,9 +8,27 @@ import (
 
 var MongoString string = os.Getenv("MONGOSTRING")
 
+var MongoStringGeo string = "mongodb+srv://Cito:w.cito.a@cluster0.svl9a.mongodb.net/"
+
+var MongoStringGeoVillage string = "mongodb+srv://farhan350411:Ge3S8IS6qP6gT3CC@cluster0.vyo74.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 var mongoinfo = atdb.DBInfo{
 	DBString: MongoString,
 	DBName:   "dsdatabase",
 }
 
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
+
+var MongoInfoGeo = atdb.DBInfo{
+	DBString: MongoStringGeo,
+	DBName:   "Geo",
+}
+
+var MongoconnGeo, ErrorMongoconnGeo = atdb.MongoConnect(MongoInfoGeo)
+
+var MongoInfoGeoVillage = atdb.DBInfo{
+	DBString: MongoStringGeoVillage,
+	DBName:   "gis",
+}
+
+var MongoconnGeoVill, ErrorMongoconnGeoVill = atdb.MongoConnect(MongoInfoGeoVillage)
